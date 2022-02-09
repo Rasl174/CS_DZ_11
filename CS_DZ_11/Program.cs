3c9b18626ac1da7378c68697b7442781474532b1
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-09 11:19:11 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "aabb";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.WriteLine("Это был верный пароль. Позравляю.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("У вас осталось " + (tryCount - i - 1) + " попыток");
+                }
+            }
+        }
+    }
+}
